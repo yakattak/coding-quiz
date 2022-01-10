2ad903ad130834b916997da3ab67258303dbbb49
yakattak <stevecalebrese@gmail.com>
2022-01-10 13:20:12 -0500

    begin function to create questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
 var questionBoxEl = document.querySelector(".question-container");
+var questionPhraseEl = document.querySelector(".question-phrase")
+var choiceListEl = document.querySelector(".choice-list");
 
 var timerEl = document.getElementById('countdown');
 var setTimer = 75;
-
+questionBank = [
+  question1 = {
+    question: "this is the question",
+    choices : {
+    correct: "this is choice 1",
+    false1: "this is false choice 1",
+    false2: "this is false choice 2",
+    false3: "this is false choice 3",
+        }, 
+    },
+];
 //Timer Countdown
 
 function readButton(event) {
+    event.preventDefault();
     var targetEl = event.target;
     
   //begin quiz if start button clicked
@@ -17,6 +30,7 @@ function readButton(event) {
 
 //function to start timer and begin quiz
 var beginquiz = function(targetEl) {
+    
 
         var timeLeft = setTimer;
         var timeInterval = setInterval(function() {
@@ -27,11 +41,23 @@ var beginquiz = function(targetEl) {
             
         }, 1000);
         targetEl.remove();
-        loadQuestions();
+        questionNum = 0;
+        loadQuestion();
 }
 
-var loadQuestions = function() {
+var createQuestionForm = function(questionNum) {
+    for (i in questionBank[questionNum].choices) {
+        console.log("yeet");
+
+    }
     
+}
+
+var loadQuestion = function() {
+    
+    createQuestionForm(questionNum)
+
+  
 }
 
 
